cba589851c415b38d1fc9551c6309639aaf08327
wangyan1998 <1011344314@qq.com>
2022-03-04 18:34:07 +0800

    hot100

--- src/algorithm/SearchMatrix.java
+++ src/algorithm/SearchMatrix.java
@@ -1,4 +1,14 @@
 package algorithm;
+//编写一个高效的算法来搜索mxn矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+//        每行的元素从左到右升序排列。
+//        每列的元素从上到下升序排列。
+//        m == matrix.length
+//        n == matrix[i].length
+//        1 <= n, m <= 300
+//        -10^9<= matrix[i][j] <= 10^9
+//        每行的所有元素从左到右升序排列
+//        每列的所有元素从上到下升序排列
+//        -10^9<= target <= 10^9
 
 /**
  * @author wy
